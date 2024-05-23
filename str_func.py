3790ff26fce1a23925ccd83c750c6ecfc1e2c233
Olga Noskova <olga.tolba@gmail.com>
2024-05-24 08:09:17 +1000

    Дописали функцию str_title, добавили dostrings

--- str_func.py
+++ str_func.py
@@ -1,5 +1,18 @@
 def str_func(s):
+    """
+    Функция возвращает строку со всеми заглавными буквами
+    """
     return s.upper()
 
 
 print(str_func('Hello world!'))
+
+
+def str_title(s):
+     """
+     Функция возвращает заглавными первые буквы каждого слова
+     """
+     return s.title()
+
+
+print(str_title('hello world!'))
